cb03180c5ea69a6586d628d67b267110610f03f3
Darkas20 <vladpochenok@gmail.com>
2018-10-02 15:51:23 +0300

    Added Empty movies message

--- frontend/src/components/MoviesList.jsx
+++ frontend/src/components/MoviesList.jsx
@@ -4,13 +4,25 @@ import PropTypes from 'prop-types';
 
 const MoviesList = ({ movies, deleteMovie }) => (
 	<div className="ui three cards stackable">
-		{movies.map(movie => (
-			<MovieCard
-				key={movie._id}
-				movie={movie}
-				deleteMovie={deleteMovie}
-			/>
-		))}
+		{movies.length === 0 ? (
+			<div className="ui icon message">
+				<i className="icon info" />
+				<div className="content">
+					<div className="header">
+						There are no movies in your store!
+					</div>
+					<p>You should add some, don't you think?</p>
+				</div>
+			</div>
+		) : (
+			movies.map(movie => (
+				<MovieCard
+					key={movie._id}
+					movie={movie}
+					deleteMovie={deleteMovie}
+				/>
+			))
+		)}
 	</div>
 );
 
